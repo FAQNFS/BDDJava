447a2546832ca6fdb8fc597ee9896abfb301ada7
Nikita Troshin <nikitas.troshin@mail.ru>
2023-10-25 17:09:19 +0300

    Modified DataHelper

--- src/main/java/ru/netology/test/MoneyTransferTest.java
+++ src/main/java/ru/netology/test/MoneyTransferTest.java
@@ -47,7 +47,7 @@ public class MoneyTransferTest {
         var amount = generateInvalidAmount(secondCardBalance);
         var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
         transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
-        transferPage.findErrorMessage("Выполнена попыька перевода суммы, превышающей остаток на карте списания");
+        transferPage.findErrorMessage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
         var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
         var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
         assertEquals(firstCardBalance, actualBalanceFirstCard);
